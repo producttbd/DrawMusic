9b02930f55cc174a1ba672d6bbef4143e47f93ab
Trevor Knight <trevorknight@gmail.com>
2015-11-28 11:47:14 -0500

    Noise brush

--- Source/BrushFactory.cpp
+++ Source/BrushFactory.cpp
@@ -8,7 +8,8 @@ OwnedArray<PixelBrush> BrushFactory::getAllBrushes()
     OwnedArray<PixelBrush> result;
     Array<BrushPoint> brushPattern;
 
-    brushPattern.clear();
+
+    // Square brush
     brushPattern.add(BrushPoint(-1, -1, 0.2, PointType::Additive));
     brushPattern.add(BrushPoint(-1, 0, 0.2, PointType::Additive));
     brushPattern.add(BrushPoint(-1, 1, 0.2, PointType::Additive));
@@ -20,10 +21,12 @@ OwnedArray<PixelBrush> BrushFactory::getAllBrushes()
     brushPattern.add(BrushPoint(0, 0, 0.2, PointType::Additive));
     result.add(new PointClusterBrush(TRANS("Square brush"), brushPattern));
 
+    // Hann brush
     Array<float> hannProfile({0.1464466094067262f, 0.49999999999999994f, 0.8535533905932737f,
         1.0f, 0.8535533905932738f, 0.5000000000000001f, 0.14644660940672627f});
     result.add(new XYProfileBrush(TRANS("Hann point"), hannProfile, hannProfile, -3, -3));
 
+    // Percussion
     Array<float> xPercussive({0.65f,
         1.0f, 0.8535533905932738f, 0.5000000000000001f, 0.14644660940672627f});
     Array<float> yPercussive;
@@ -31,8 +34,19 @@ OwnedArray<PixelBrush> BrushFactory::getAllBrushes()
     {
         yPercussive.add(i / 500.0f);
     }
-
     result.add(new XYProfileBrush(TRANS("Percussion"), xPercussive, yPercussive, -1, 0));
+
+    // noise cluster
+    brushPattern.clear();
+    Random random;
+    for (int x = -40; x <= 40; ++x)
+    {
+        for (int y = -40; y <= 40; ++y)
+        {
+            brushPattern.add(BrushPoint(x, y, random.nextFloat()*0.1f, PointType::Absolute));
+        }
+    }
+    result.add(new PointClusterBrush(TRANS("Noise brush"), brushPattern));
 
     return result;
 }
