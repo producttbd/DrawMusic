f0bba00092599deea97de99c6b1cc9ecdd872f93
Trevor Knight <trevorknight@gmail.com>
2015-12-28 12:49:49 -0500

    Add PlaybackTimeline

---
+++ Source/PlaybackTimeline.h
@@ -0,0 +1,13 @@
+#ifndef PLAYBACKTIMELINE_H_INCLUDED
+#define PLAYBACKTIMELINE_H_INCLUDED
+
+#include "JuceHeader.h"
+
+public class PlaybackTimeline : public Component
+{
+    
+};
+
+
+
+#endif  // PLAYBACKTIMELINE_H_INCLUDED
